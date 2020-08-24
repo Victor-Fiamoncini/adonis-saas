4a36a64ef7d3d04fe61c4457ff1f4b177e3e490e
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-08-24 20:38:43 -0300

    feat: session JWT & Team CRUD

--- server/database/seeds/DatabaseSeeder.js
+++ server/database/seeds/DatabaseSeeder.js
@@ -12,10 +12,21 @@
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
+const User = use('App/Models/User')
 
 class DatabaseSeeder {
-  async run () {
-  }
+	async run() {
+		const user = await User.create({
+			name: 'Victor Fiamoncini',
+			email: 'victor@mail.com',
+			password: '123456',
+		})
+
+		const team = await user.teams().create({
+			name: 'NodeUS',
+			user_id: user.id,
+		})
+	}
 }
 
 module.exports = DatabaseSeeder
