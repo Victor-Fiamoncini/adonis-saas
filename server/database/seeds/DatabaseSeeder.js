db2c849d5a6c059b7940d6560024938bbd96c917
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-08-25 21:07:18 -0300

    feat: user_team roles & permissions

--- server/database/seeds/DatabaseSeeder.js
+++ server/database/seeds/DatabaseSeeder.js
@@ -10,9 +10,9 @@
 |
 */
 
-/** @type {import('@adonisjs/lucid/src/Factory')} */
-const Factory = use('Factory')
 const User = use('App/Models/User')
+const Role = use('Adonis/Acl/Role')
+const Permission = use('Adonis/Acl/Permission')
 
 class DatabaseSeeder {
 	async run() {
@@ -22,10 +22,42 @@ class DatabaseSeeder {
 			password: '123456',
 		})
 
+		const createInvite = await Permission.create({
+			slug: 'invites_create',
+			name: 'Convidar membros',
+		})
+
+		const createProject = await Permission.create({
+			slug: 'projects_create',
+			name: 'Criar projetos',
+		})
+
+		const admin = await Role.create({
+			slug: 'administrator',
+			name: 'Administrador',
+		})
+
+		const moderator = await Role.create({
+			slug: 'moderator',
+			name: 'Moderador',
+		})
+
+		await Role.create({
+			slug: 'visitor',
+			name: 'Visitante',
+		})
+
+		await admin.permissions().attach([createInvite.id, createProject.id])
+		await moderator.permissions().attach([createProject.id])
+
 		const team = await user.teams().create({
 			name: 'NodeUS',
 			user_id: user.id,
 		})
+
+		const teamJoin = await user.teamJoins().where('team_id', team.id).first()
+
+		await teamJoin.roles().attach([admin.id])
 	}
 }
 
